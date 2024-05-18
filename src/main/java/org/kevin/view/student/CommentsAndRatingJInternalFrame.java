1d02146f7d2b1cea9c4a74153fb48adbabf706c2
Kevin-YichenLi <2358068@edu.vaniercollege.qc.ca>
2024-05-18 15:54:37 -0400

    View comments and ratings version 2.0 implemented

--- src/main/java/org/kevin/view/student/CommentsAndRatingJInternalFrame.java
+++ src/main/java/org/kevin/view/student/CommentsAndRatingJInternalFrame.java
@@ -4,10 +4,15 @@
 
 package org.kevin.view.student;
 
+import java.awt.event.*;
+import org.kevin.dao.NovelDao;
 import org.kevin.dto.Novel;
+import org.kevin.util.DBUtil;
 
 import javax.swing.*;
 import javax.swing.GroupLayout;
+import java.sql.Connection;
+import java.sql.ResultSet;
 
 /**
  * @author kevin
@@ -15,6 +20,10 @@ import javax.swing.GroupLayout;
 public class CommentsAndRatingJInternalFrame extends JInternalFrame {
     private String type;
     private int id;
+    private int index;
+    private int length;
+    private DBUtil dbUtil = new DBUtil();
+    private NovelDao novelDao = new NovelDao();
     public CommentsAndRatingJInternalFrame(String type, int id) {
         initComponents();
         setSize(580, 435);
@@ -22,6 +31,22 @@ public class CommentsAndRatingJInternalFrame extends JInternalFrame {
         this.type = type;
         this.id = id;
         showAverageRating();
+        index = 0;
+        showComment();
+    }
+
+    private void onNextPressed(ActionEvent e) {
+        if (index != length - 1) {
+            index++;
+            showComment();
+        }
+    }
+
+    private void onPreviousPressed(ActionEvent e) {
+        if (index != 0) {
+            index--;
+            showComment();
+        }
     }
 
     private void initComponents() {
@@ -33,6 +58,7 @@ public class CommentsAndRatingJInternalFrame extends JInternalFrame {
         ratingTxt = new JTextField();
         scrollPane1 = new JScrollPane();
         commentTxt = new JTextArea();
+        label2 = new JLabel();
 
         //======== this ========
         setVisible(true);
@@ -44,9 +70,11 @@ public class CommentsAndRatingJInternalFrame extends JInternalFrame {
 
         //---- button1 ----
         button1.setText("Previous");
+        button1.addActionListener(e -> onPreviousPressed(e));
 
         //---- button2 ----
         button2.setText("Next");
+        button2.addActionListener(e -> onNextPressed(e));
 
         //---- label1 ----
         label1.setText("Average rating:");
@@ -61,6 +89,9 @@ public class CommentsAndRatingJInternalFrame extends JInternalFrame {
             commentTxt.setEditable(false);
             scrollPane1.setViewportView(commentTxt);
         }
+
+        //---- label2 ----
+        label2.setText("Comments:");
 
         GroupLayout contentPaneLayout = new GroupLayout(contentPane);
         contentPane.setLayout(contentPaneLayout);
@@ -77,6 +108,8 @@ public class CommentsAndRatingJInternalFrame extends JInternalFrame {
                     .addGroup(contentPaneLayout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
                         .addGroup(contentPaneLayout.createSequentialGroup()
                             .addComponent(button1)
+                            .addGap(117, 117, 117)
+                            .addComponent(label2)
                             .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(button2))
                         .addComponent(scrollPane1, GroupLayout.PREFERRED_SIZE, 467, GroupLayout.PREFERRED_SIZE))
@@ -92,7 +125,8 @@ public class CommentsAndRatingJInternalFrame extends JInternalFrame {
                     .addGap(17, 17, 17)
                     .addGroup(contentPaneLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
                         .addComponent(button1)
-                        .addComponent(button2))
+                        .addComponent(button2)
+                        .addComponent(label2))
                     .addGap(18, 18, 18)
                     .addComponent(scrollPane1, GroupLayout.PREFERRED_SIZE, 238, GroupLayout.PREFERRED_SIZE)
                     .addContainerGap(28, Short.MAX_VALUE))
@@ -108,8 +142,81 @@ public class CommentsAndRatingJInternalFrame extends JInternalFrame {
     private JTextField ratingTxt;
     private JScrollPane scrollPane1;
     private JTextArea commentTxt;
+    private JLabel label2;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
-    private void showAverageRating() {
+    private void showComment() {
+        if (type.equals("textbook")) {
 
+        } else {
+            Connection con = null;
+            try {
+                con = dbUtil.getCon();
+                ResultSet resultSet = novelDao.list(con, new Novel());
+
+                int counter = 0;
+                String originalComments = "";
+
+                while (resultSet.next()) {
+                    String currentId = resultSet.getString("id");
+                    if (id == Integer.parseInt(currentId)) {
+                        originalComments = resultSet.getString("comments");
+                    }
+                }
+
+                String[] comments = originalComments.split("~");
+                length = comments.length;
+                commentTxt.setText(comments[index]);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    dbUtil.closeCon(con);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void showAverageRating() {
+        if (type.equals("textbook")) {
+
+        } else {
+            Connection con = null;
+            try {
+                con = dbUtil.getCon();
+                ResultSet resultSet = novelDao.list(con, new Novel());
+
+                double rating = 0;
+                int counter = 0;
+                String comments = "";
+
+                while (resultSet.next()) {
+                    String currentId = resultSet.getString("id");
+                    if (id == Integer.parseInt(currentId)) {
+                        rating = Double.parseDouble(resultSet.getString("rating"));
+                        comments = resultSet.getString("comments");
+                    }
+                }
+
+                for (int i = 0; i < comments.length(); i++) {
+                    if (comments.charAt(i) == '~') {
+                        counter++;
+                    }
+                }
+
+                rating /= counter;
+                ratingTxt.setText(rating + "");
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    dbUtil.closeCon(con);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
